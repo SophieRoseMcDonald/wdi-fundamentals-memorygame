ebaa2405997db2788ccf34a058e494d397b34482
Sophie McDonald <sophiermcdonald@gmail.com>
2017-11-22 23:21:56 +1100

    Fix indentation :cop:

--- js/main.js
+++ js/main.js
@@ -1,62 +1,61 @@
-var cards = [
-	{
-		rank: 'queen',
-		suit: 'hearts',
-		cardImage: 'images/queen-of-hearts.png'
-	},
-	{
-		rank: 'queen',
-		suit: 'diamonds',
-		cardImage: 'images/queen-of-diamonds.png'
-	},
-	{
-		rank: 'king',
-		suit: 'hearts',
-		cardImage: 'images/king-of-hearts.png'
-	},
-	{
-		rank: 'king',
-		suit: 'diamonds',
-		cardImage: 'images/king-of-diamonds.png'
-	}
+var cards = [{
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    rank: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
 ];
 
 var cardsInPlay = [];
 
 var checkForMatch = function(cardId) {
-	if (cardsInPlay.length === 2) {
-		var card = cards[cardId];
-		this.setAttribute('src', card[cardId].cardImage);
+  if (cardsInPlay.length === 2) {
+    var card = cards[cardId];
+    this.setAttribute('src', card[cardId].cardImage);
     if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert('You found a match!');
+      alert('You found a match!');
     } else {
-    alert('Sorry, try again.');
+      alert('Sorry, try again.');
     }
-	}
-	};
+  }
+};
 
 var flipCard = function() {
-	var cardId = this.getAttribute('data-id');
-    console.log('User flipped ' + cards[cardId].rank)
+  var cardId = this.getAttribute('data-id');
+  console.log('User flipped ' + cards[cardId].rank)
 
-    cardsInPlay.push(cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
 
-		console.log(cards[cardId].suit);
-		console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
 
-    checkForMatch(cardId);
+  checkForMatch(cardId);
 };
 
 var createBoard = function() {
-	for (var i = 0; i < cards.length; i++) {
-		var cardElement = document.createElement('img');
-		 cardElement.setAttribute('src', 'images/back.png');
-		 cardElement.setAttribute('data-id', i);
-		 cardElement.addEventListener('click', flipCard);
-		 var board = document.getElementById('game-board');
-        board.appendChild(cardElement);
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    var board = document.getElementById('game-board');
+    board.appendChild(cardElement);
 
-		}
-	};
+  }
+};
 
 createBoard();
