17229a8a58f45d8e6d358187be3a4ff86137db51
Sophie McDonald <sophiermcdonald@gmail.com>
2017-11-22 15:46:36 +1100

    Add JS cardsInPlay

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo, cardOne);
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert ("You've found a match");
+    } else {
+        alert ("Sorry. Try again!");
+    }
+}
 
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardsInPlay);
